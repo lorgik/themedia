94b076a10093692a2d7c5973721dac442ee3994d
lorgik <86115556+lorgik@users.noreply.github.com>
2025-04-26 19:13:48 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const servicesMenu = document.querySelector(".services__menu")
 const servicesMenuBtn = document.querySelector(".menu__item--services")
 
-servicesMenuBtn.addEventListener("click", () => {
-    servicesMenu.classList.toggle("active")
-    servicesMenuBtn.classList.toggle("active")
+servicesMenuBtn.addEventListener("mouseenter", () => {
+    servicesMenu.classList.add("active")
+    servicesMenuBtn.classList.add("active")
+})
+
+servicesMenuBtn.addEventListener("mouseleave", () => {
+    setTimeout(() => {
+        if (!servicesMenu.matches(":hover")) {
+            servicesMenu.classList.remove("active")
+            servicesMenuBtn.classList.remove("active")
+        }
+    }, 100)
+})
+servicesMenu.querySelector(".container").addEventListener("mouseleave", () => {
+    setTimeout(() => {
+        if (!servicesMenu.querySelector(".container").matches(":hover")) {
+            servicesMenu.classList.remove("active")
+            servicesMenuBtn.classList.remove("active")
+        }
+    }, 100)
 })
 
 const mainForm = document.querySelector(".main-form")
 mainForm.addEventListener("submit", (event) => {
     const mainFormInput = mainForm.querySelector(".phone-input")
-
-    console.log(mainFormInput.value)
 
     if (!validatePhoneNumber(mainFormInput.value)) {
         event.preventDefault()
@@ -21,3 +36,17 @@ function validatePhoneNumber(input) {
     const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
     return regex.test(input)
 }
+
+window.addEventListener(
+    "scroll",
+    () => {
+        document.body.style.setProperty("--scroll", window.scrollY / (document.body.offsetHeight - window.innerHeight))
+    },
+    false
+)
+
+const signs = document.querySelectorAll(".sign")
+
+signs.forEach((sign) => {
+    sign.style.setProperty("--speed", `${(0.5 * sign.getAttribute("data-speed")) / 2}`)
+})
